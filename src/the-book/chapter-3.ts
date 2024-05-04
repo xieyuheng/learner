bd310253b0d14be654e73561388dbb3a2754ddc6
Xie Yuheng <xyheme@gmail.com>
2024-05-05 00:52:12 +0800

    Parameters should not be Tensor but Array of number

--- src/the-book/chapter-3.ts
+++ src/the-book/chapter-3.ts
@@ -1,13 +1,12 @@
 import { type Tensor } from "./chapter-2.js"
 import { square, sub, sum } from "./interlude-i.js"
 
-export function l2Loss(
-  target: (xs: Tensor) => (ps: any) => Tensor,
-): (xs: Tensor, ys: Tensor) => (ps: Tensor) => number {
+export function l2Loss<Parameters extends Array<number>>(
+  target: (xs: Tensor) => (ps: Parameters) => Tensor,
+): (xs: Tensor, ys: Tensor) => (ps: Parameters) => number {
   return function expectant(xs, ys) {
     return function objective(ps) {
       const predYs = target(xs)(ps)
-      // TODO `l2Loss` can only take `Tensor1`.
       return sum(square(sub(ys, predYs))) as number
     }
   }
