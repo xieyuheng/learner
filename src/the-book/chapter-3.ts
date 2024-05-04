8435140039c5d9d7d0fe396fe1b36f09dd1a75a2
Xie Yuheng <xyheme@gmail.com>
2024-05-04 22:16:25 +0800

    up

--- src/the-book/chapter-3.ts
+++ src/the-book/chapter-3.ts
@@ -5,7 +5,7 @@ export function l2Loss(
   target: (xs: Tensor) => (θ: any) => Tensor,
 ): (xs: Tensor, ys: Tensor) => (θ: Tensor) => number {
   return function expectant(xs, ys) {
-    return function objective(θ: Tensor) {
+    return function objective(θ) {
       const predYs = target(xs)(θ)
       // TODO `l2Loss` can only take `Tensor1`.
       return sum(square(sub(ys, predYs))) as number
