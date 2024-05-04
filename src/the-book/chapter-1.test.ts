20e6fbfcaf7d5613f6b203cf4a5db8cb3dca93eb
Xie Yuheng <xyheme@gmail.com>
2024-05-04 16:32:37 +0800

    rename ch to chapter -- for interlude-i

--- src/the-book/chapter-1.test.ts
+++ src/the-book/chapter-1.test.ts
@@ -1,6 +1,6 @@
 import assert from "node:assert"
 import { test } from "node:test"
-import { line } from "./ch1.js"
+import { line } from "./chapter-1.js"
 
 test("ch1 -- line", () => {
   assert.deepStrictEqual(line(1)([2, 3]), 2 * 1 + 3)
--- src/the-book/chapter-2.test.ts
+++ src/the-book/chapter-2.test.ts
@@ -1,6 +1,6 @@
 import assert from "node:assert"
 import { test } from "node:test"
-import { shape } from "./ch2.js"
+import { shape } from "./chapter-2.js"
 
 test("ch2 -- shape", () => {
   assert.deepStrictEqual(shape(1), [])
