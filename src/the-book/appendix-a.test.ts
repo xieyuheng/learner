c469ed7a3e5832a0a60ab9d5646d55da7deec465
Xie Yuheng <xyheme@gmail.com>
2024-05-08 20:33:14 +0800

    appendix-a -- test `scalarTruncate`

--- src/the-book/appendix-a.test.ts
+++ src/the-book/appendix-a.test.ts
@@ -1,6 +1,6 @@
 import assert from "node:assert"
 import { test } from "node:test"
-import { tensorMap } from "./appendix-a.js"
+import { tensorMap, Dual, endOfChain, scalarTruncate } from "./appendix-a.js"
 
 test("appendix-a -- tensorMap", () => {
   assert.deepStrictEqual(
@@ -14,6 +14,22 @@ test("appendix-a -- tensorMap", () => {
     [
       [2, 3, 4],
       [5, 6, 7],
+    ],
+  )
+})
+
+test("appendix-a -- scalarTruncate", () => {
+  assert.deepStrictEqual(
+    tensorMap(
+      scalarTruncate,
+      [
+        [1, 2, 3],
+        [4, 5, 6],
+      ],
+    ),
+    [
+      [Dual(1, endOfChain), Dual(2, endOfChain), Dual(3, endOfChain)],
+      [Dual(4, endOfChain), Dual(5, endOfChain), Dual(6, endOfChain)],
     ],
   )
 })
