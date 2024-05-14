9fbb5cb300065e3f5825ea658d7a64d75063e6eb
Xie Yuheng <xyheme@gmail.com>
2024-05-15 02:40:06 +0800

    up

--- src/system-a/revise.test.ts
+++ src/system-a/revise.test.ts
@@ -4,7 +4,7 @@ import { revise } from "./revise.js"
 
 test("revise", () => {
   function fn(ps: Array<number>): Array<number> {
-    return ps.map(p => p - 3)
+    return ps.map((p) => p - 3)
   }
 
   assert.deepStrictEqual(revise(fn, 5, [1, 2, 3]), [-14, -13, -12])
