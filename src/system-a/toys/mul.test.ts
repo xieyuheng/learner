1ac027b54c7b1c2564528e56a2d3088d79004777
Xie Yuheng <xyheme@gmail.com>
2024-05-26 23:47:19 +0800

    fix tests about extend and mul

--- src/system-a/toys/mul.test.ts
+++ src/system-a/toys/mul.test.ts
@@ -1,22 +1,41 @@
+import assert from "node:assert"
 import { test } from "node:test"
+import { tensorReal } from "../tensor/tensorReal.js"
 import { mul } from "./index.js"
 
+// NOTE Related issues about `ext2`:
+// https://github.com/themetaschemer/malt/issues/56
+
 test("mul -- extended", () => {
-  console.log(
-    mul(
-      [
-        [1, 2],
-        [3, 4],
-      ],
-      [5, 6],
+  assert.deepStrictEqual(
+    tensorReal(
+      mul(
+        [
+          [1, 2],
+          [3, 4],
+        ],
+        [5, 6],
+      ),
     ),
+    [
+      [5, 12],
+      [15, 24],
+    ],
   )
-  // assert.deepStrictEqual(
-  //   tensorReal(
-  //     mul(
-  //       (* (tensor (tensor 1 2) (tensor 3 4)) (tensor 5 6))
-  //       (tensor 5 6)),
-  //   ),
-  //   [6, 15],
-  // )
+
+  assert.deepStrictEqual(
+    tensorReal(
+      mul(
+        [
+          [3, 4, 5],
+          [7, 8, 9],
+        ],
+        [2, 4, 3],
+      ),
+    ),
+    [
+      [6, 16, 15],
+      [14, 32, 27],
+    ],
+  )
 })
