bfb1a5a8a66f43e20be03e763c3cebcb169cac67
Xie Yuheng <xyheme@gmail.com>
2024-05-29 01:59:53 +0800

    up

--- src/system-a/models/irisDataset.ts
+++ src/system-a/models/irisDataset.ts
@@ -2,7 +2,7 @@ const setosa = [1.0, 0.0, 0.0]
 const versicolor = [0.0, 1.0, 0.0]
 const virginica = [0.0, 0.0, 1.0]
 
-export const irisDataset = [
+export const irisFull = [
   [[5.1, 3.5, 1.4, 0.2], setosa],
   [[4.9, 3.0, 1.4, 0.2], setosa],
   [[4.7, 3.2, 1.3, 0.2], setosa],
