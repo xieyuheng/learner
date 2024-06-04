174af036055c13cb34b82200b5ea11901316f115
Xie Yuheng <xyheme@gmail.com>
2024-06-04 20:25:32 +0800

    up

--- src/system-a/gradient-descent/gradient.test.ts
+++ src/system-a/gradient-descent/gradient.test.ts
@@ -20,4 +20,14 @@ test("gradient -- mul", () => {
   assert.deepStrictEqual(gradient(mul, [1, 1]), [1, 1])
   assert.deepStrictEqual(gradient(mul, [2, 3]), [3, 2])
   assert.deepStrictEqual(gradient(mul, [[2], [3]]), [[3], [2]])
+  assert.deepStrictEqual(
+    gradient(mul, [
+      [1, 2],
+      [3, 4],
+    ]),
+    [
+      [3, 4],
+      [1, 2],
+    ],
+  )
 })
